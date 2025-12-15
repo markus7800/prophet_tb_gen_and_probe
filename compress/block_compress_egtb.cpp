c7dca4d6ae33f0d048ddbef40374aa195187f740
markus7800 <markus.boeck7800@hotmail.com>
2025-12-15 11:26:02 +0100

    zstd experiment

--- compress/block_compress_egtb.cpp
+++ compress/block_compress_egtb.cpp
@@ -14,9 +14,12 @@
 #include <sys/stat.h> // file size
 #include "../misc.h"
 namespace fs = std::filesystem;
-#include "libdeflate.h"
 
-uint64_t compress_egtb(std::string filename, int nthreads, int compression_level, uint64_t block_size) {
+#include "libdeflate.h"
+#include <zstd.h>
+
+
+uint64_t compress_egtb(std::string filename, int nthreads, int compression_level, uint64_t block_size, bool write) {
 
   // load file
   FILE* f = fopen(filename.c_str(), "rb");
@@ -46,7 +49,7 @@ uint64_t compress_egtb(std::string filename, int nthreads, int compression_level
   #pragma omp parallel num_threads(nthreads)
   {
     // thread-private allocations
-    libdeflate_compressor* compressor = libdeflate_alloc_compressor(compression_level);
+    // libdeflate_compressor* compressor = libdeflate_alloc_compressor(compression_level);
     std::vector<uint8_t> compressed(block_size*2);
 
     
@@ -55,7 +58,8 @@ uint64_t compress_egtb(std::string filename, int nthreads, int compression_level
       uint64_t size = std::min(block_size, num_pos - start_ix) * sizeof(int16_t);
       uint64_t block_ix = start_ix / block_size;
 
-      uint64_t compressed_size = libdeflate_deflate_compress(compressor, (uint8_t*) &TB[start_ix], size, compressed.data(), compressed.size());
+      // uint64_t compressed_size = libdeflate_deflate_compress(compressor, (uint8_t*) &TB[start_ix], size, compressed.data(), compressed.size());
+      uint64_t compressed_size = ZSTD_compress(compressed.data(), compressed.size(),  (uint8_t*) &TB[start_ix], size, compression_level);
 
       if (compressed_size == 0) {
         std::cerr << "Compression failed\n";
@@ -65,11 +69,12 @@ uint64_t compress_egtb(std::string filename, int nthreads, int compression_level
       final_size += compressed_size;
 
       assert (compressed_size <= UINT16_MAX);
+      assert (compressed_size <= size);
       block_sizes[block_ix] = compressed_size;
       std::memcpy(&TB[start_ix], compressed.data(), compressed_size);
     }
 
-    libdeflate_free_compressor(compressor);
+    // libdeflate_free_compressor(compressor);
   }
 
   final_size += large_blocks ? sizeof(uint32_t) * n_blocks : sizeof(uint16_t) * n_blocks;
@@ -78,45 +83,28 @@ uint64_t compress_egtb(std::string filename, int nthreads, int compression_level
 
   std::cout << "final size: " << final_size << " " << (double) final_size / file_size << " (large_blocks = " << large_blocks << ")" << std::endl;
 
-  std::string compressed_filename = filename + ".bd";
-  f = fopen(compressed_filename.c_str(), "wb");
-  if (f == NULL) {
-    std::cerr << "Error opening file: " << filename << std::endl;
-    exit(1);
+
+  if (write) {
+    std::string compressed_filename = filename + ".bz";
+    f = fopen(compressed_filename.c_str(), "wb");
+    if (f == NULL) {
+      std::cerr << "Error opening file: " << filename << std::endl;
+      exit(1);
+    }
+    fwrite(&num_pos, sizeof(uint64_t), 1, f);
+    fwrite(&block_size, sizeof(uint64_t), 1, f);
+    fwrite(block_sizes, sizeof(uint16_t), n_blocks, f);
+
+    for (uint64_t start_ix = 0; start_ix < num_pos; start_ix += block_size) {
+      uint64_t block_ix = start_ix / block_size;
+      fwrite(&TB[start_ix], sizeof(uint8_t), block_sizes[block_ix], f);
+      assert(block_ix * block_size == start_ix);
+    }
+    std::cout << "Wrote to " << compressed_filename << std::endl;
+    fclose(f);
   }
-  fwrite(&num_pos, sizeof(uint64_t), 1, f);
-  fwrite(&block_size, sizeof(uint64_t), 1, f);
-  fwrite(block_sizes, sizeof(uint16_t), n_blocks, f);
-
-  // libdeflate_decompressor* decompressor = libdeflate_alloc_decompressor();
-  // int16_t* uncompressed_buf = (int16_t*) malloc(block_size * sizeof(int16_t));
-
-  for (uint64_t start_ix = 0; start_ix < num_pos; start_ix += block_size) {
-    uint64_t block_ix = start_ix / block_size;
-    fwrite(&TB[start_ix], sizeof(uint8_t), block_sizes[block_ix], f);
-    assert(block_ix * block_size == start_ix);
-    // uint64_t size = std::min(block_size, num_pos - block_ix*block_size) * sizeof(int16_t);
-    // std::cout << start_ix << " " << size << " -> " <<  block_sizes[block_ix] << std::endl;
-
-    // if (start_ix == 0) {
-    //   for (int i = 0; i < 10; i++ ) std::cout << int(((uint8_t*) &TB[start_ix])[i]) << " ";
-    //   std::cout << std::endl;
-    // }
-    // 
-    // libdeflate_result res = libdeflate_deflate_decompress(decompressor,
-    //   &TB[start_ix], block_sizes[block_ix],
-    //   uncompressed_buf, size, NULL);
-    // if (res != 0) {
-    //   std::cout << "libdeflate_result: " << res << std::endl;
-    //   exit(1);
-    // }
-  }
-  // libdeflate_free_decompressor(decompressor);
-  std::cout << "Wrote to " << compressed_filename << std::endl;
-  fclose(f);
 
   free(TB);
-
   return final_size;
 }
 
@@ -157,9 +145,6 @@ struct CompressedEGTB {
     this->n_blocks = ceil((double) num_pos / block_size);
     this->block_sizes = (uint16_t*) &this->map_ptr[2*sizeof(uint64_t)];
     this->compressed_blocks = &this->map_ptr[2*sizeof(uint64_t) + this->n_blocks*sizeof(uint16_t)];
-    // for (int i = 0; i < 10; i++ ) std::cout << int(compressed_blocks[i]) << " ";
-    // std::cout << std::endl;
-
     this->block_offsets = (uint64_t*) malloc(this->n_blocks * sizeof(uint64_t));
     this->uncompressed_buf = (int16_t*) malloc(block_size * sizeof(int16_t));
 
@@ -169,7 +154,7 @@ struct CompressedEGTB {
       offset += this->block_sizes[block_ix];
     }
 
-    // this->decompressor = libdeflate_alloc_decompressor();
+    this->decompressor = libdeflate_alloc_decompressor();
 
     std::cout << "CompressedEGTB: filesize=" << this->filesize << ", num_pos=" << this->num_pos << ", block_size=" << this->block_size << ", n_blocks=" << this->n_blocks << std::endl;
   }
@@ -182,23 +167,23 @@ struct CompressedEGTB {
     }
     free(this->block_offsets);
     free(this->uncompressed_buf);
-    // libdeflate_free_decompressor(this->decompressor);
+    libdeflate_free_decompressor(this->decompressor);
   }
 
   int16_t get(uint64_t ix) {
-    decompressor = libdeflate_alloc_decompressor();
     uint64_t block_ix = ix / block_size;
     uint64_t ix_in_block = ix % block_size;
     uint64_t offset = block_offsets[block_ix];
-    uint64_t size = std::min(block_size, num_pos - block_ix*block_size) * sizeof(int16_t);
-    libdeflate_result res = libdeflate_deflate_decompress(decompressor,
-      &compressed_blocks[offset], block_sizes[block_ix],
-      uncompressed_buf, size, NULL);
-    if (res != 0) {
-      std::cout << "libdeflate_result: " << res << std::endl;
-      exit(1);
-    }
-    libdeflate_free_decompressor(decompressor);
+    // uint64_t size = std::min(block_size, num_pos - block_ix*block_size) * sizeof(int16_t);
+    // libdeflate_result res = libdeflate_deflate_decompress(decompressor,
+    //   &compressed_blocks[offset], block_sizes[block_ix],
+    //   uncompressed_buf, size, NULL);
+    // if (res != 0) {
+    //   std::cout << "libdeflate_result: " << res << std::endl;
+    //   exit(1);
+    // }
+    size_t res = ZSTD_decompress(uncompressed_buf, block_size * sizeof(int16_t), &compressed_blocks[offset], block_sizes[block_ix]);
+    assert (!ZSTD_isError(res));
     return uncompressed_buf[ix_in_block];
   }
 };
@@ -223,7 +208,7 @@ int main(int argc, char *argv[]) {
       total_size += size;
       uint64_t zipped_size = fs::file_size(fs::path(entry.path().u8string() + ".zip"));
       total_zipped_size += zipped_size;
-      uint64_t compressed_size = compress_egtb(entry.path().u8string(), n_threads, compression_level, block_size);
+      uint64_t compressed_size = compress_egtb(entry.path().u8string(), n_threads, compression_level, block_size, false);
       total_compressed_size += compressed_size;
       std::cout << entry.path() << " size: " << size;
       std::cout << " zipped size: " << zipped_size << " (" << (double) zipped_size / size << ")";
@@ -238,8 +223,10 @@ int main(int argc, char *argv[]) {
 
   // zip size: 8709004054 (0.144411)
 
+  // libdeflate levels = 1 ... 12
+
   // compression_level = 9 block_size = 32768 -> 8563460076 (0.141998) 3m20
-  // compression_level = 10 block_size = 32768 -> 7936397012 (0.1316) 7m
+  // compression_level = 10 block_size = 32768 -> 7936397012 (0.1316) 7m BEST
   // compression_level = 12 block_size = 32768 -> 7785773584 (0.129102) 19m
 
   // compression_level = 9 block_size = 8192 -> 9137047977 (0.151509) 3m
@@ -248,14 +235,20 @@ int main(int argc, char *argv[]) {
 
   // compression_level = 10 block_size = 1048576 ->  7627181263 (0.126473) 8m
 
+  // zstd levels = 1 ... 22
+
+  // compression_level = 10 block_size = 32768 -> 8401179133 (0.139307) 2m
+  // compression_level = 16 block_size = 32768 -> 7264373856 (0.120457) 12m
+
   } else {
   
     assert(argc == 5);
     std::string filename = argv[4];
     // ../egtbs/5men/1pawns/KRKRP.egtb
 
-    compress_egtb(filename, n_threads, compression_level, block_size);
-    CompressedEGTB cegtb = CompressedEGTB(filename + ".bd");
+    compress_egtb(filename, n_threads, compression_level, block_size, true);
+
+    CompressedEGTB cegtb = CompressedEGTB(filename + ".bz");
 
     // load file
     FILE* f = fopen(filename.c_str(), "rb");
@@ -272,17 +265,15 @@ int main(int argc, char *argv[]) {
     
     fread(TB, sizeof(int16_t), num_pos, f);
     fclose(f);
-    // 294992031
+
     int n = 100000;
     TimePoint t0 = now();
     for (int i = 0; i < n; i++) {
       uint64_t ix = rand() % cegtb.num_pos;
-      // std::cout << ix << std::endl;
       assert (cegtb.get(ix) == TB[ix]);
-      // std::cout << ix << ": " << cegtb.get(ix) << std::endl;
     }
     TimePoint t1 = now();
-    std::cout << "Mean access time " << (double) (t1 - t0) /n << std::endl;
+    std::cout << "Mean access time " << (double) (t1 - t0) /n << " vs filesize " << (double) cegtb.filesize / (1024*1024*1024) <<  "GiB" << std::endl;
     free(TB);
   }
 
